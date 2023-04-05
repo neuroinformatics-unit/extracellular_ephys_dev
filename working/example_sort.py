0273cb8aefca6ebf998c49301b1f315517a6e213
Joseph Ziminski <jziminski@gpu-sr670-20.hpc.swc.ucl.ac.uk>
2023-04-05 17:47:02 +0000

    update sorter script

--- working/example_sort.py
+++ working/example_sort.py
@@ -3,9 +3,10 @@ from visualise import visualise
 import spikeinterface.sorters  as ss
 import utils
 
-base_path = r"C:\data\spike_interface"
+base_path = r"/ceph/neuroinformatics/neuroinformatics/scratch/ece_ephys_learning"
 sub_name = "1110925"
 run_name = "1110925_test_shank1"
+import os
 
 # def run_sorting(data,
 #                 sorter,
@@ -21,10 +22,14 @@ if __name__ == "__main__":
 
     rec, __ = utils.get_dict_value_from_step_num(data, "last")
 
+    # this must be run from the folder that has both sorter output AND
+    # rawdata
+    os.chdir(base_path)
+
     sorting_output = ss.run_sorter("kilosort2_5",
                                     rec,
                                     output_folder=data.sorter_output_path,
-                                    docker_image=True)
+                                    singularity_image=True)
 
     # dump some kind of config with data configs in the sorter output too
 
